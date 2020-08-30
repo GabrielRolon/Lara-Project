f1f7f1234011e640bc9f73b0867a59f262923d0d
GabrielRolon <67517159+GabrielRolon@users.noreply.github.com>
2020-08-30 18:29:57 -0300

    Estructura principal

--- tp1-archivos/main.cpp
+++ tp1-archivos/main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
 
+
 using namespace std;
+
+    struct paciente{
+    int ID;
+    char Nombres[50];
+    char Apellidos[50];
+    int Fecha;
+    float Altura;
+    float Peso;
+    char Actividad;
+    bool Aptomedico, Estado;
+    };
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+    paciente p;
+    cout<<"Ingresar ID: ";
+    cin>>p.ID;
+
+    cout<<endl<<"ingresar nombre/s: ";
+    cin.ignore();
+    cin.getline(p.Nombres,50);
+    cout<<endl<<p.Nombres<<endl;
+
+    cout<<endl<<"Ingresar Apellido/s: ";
+    cin.ignore();
+    cin.getline(p.Apellidos,50);
+
+    cout<<endl<<"ingresar fecha de nacimiento: ";
+    cin>>p.Fecha;
+
+    cout<<endl<<"ingresar Altura: ";
+    cin>>p.Altura;
+
+    cout<<endl<<"ingresar peso: ";
+    cin>>p.Peso;
+
+    cout<<endl<<"ingresar si está apto el paciente: ";
+    cin>>p.Aptomedico;
+
+
     return 0;
 }
+
+//    void menu(opcion){
+//
+//        int opcion;
+//        while(true){
+//            switch(opcion){
+//        case 1:
+//            break;//        case 2:
+//            break;//        case 3:
+//            break;//        case 4:
+//            break;//        case 0: return 0;
+//            break;
+//            }
+//        }
+//
+//
+//    };
